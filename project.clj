06a202455f192a47fc83552a543840a96d7d1c6b
MagusMachinae <Edward.Hughes1911@gmail.com>
2022-09-21 11:10:11 +0100

    [KRAK-01] update deps with new data.json, correct repo url

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject krak "0.1.1"
   :description "Kraken API"
-  :url "http://github.com/keymone/krak"
+  :url "http://github.com/HanseDigitalAccess/krak"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.0"]
                  [http-kit "2.6.0"]
                  [pandect "0.6.1"]
                  [org.clojure/data.codec "0.1.0"]
-                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/data.json "2.4.0"]
                  [javax.xml.bind/jaxb-api "2.3.1"]
                  [org.glassfish.jaxb/jaxb-runtime "2.3.1"]])
